b714b3741f5099419a62942c35aed138b917672b
AvishekHardin <avishek.hardin@gmail.com>
2014-05-04 21:32:28 +0400

    create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+element.style {
+}
+.hid {
+display: none;
+}
+canvas, caption, center, cite, code, dd, del, dfn, div, dl, dt, em, embed, fieldset, font, form, h1, h2, h3, h4, h5, h6, hr, html, i, iframe, img, ins, kbd, label, legend, li, menu, object, ol, p, pre, q, s, samp, small, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, u, ul, var {
+margin: 0;
+padding: 0;
+border: 0;
+font-size: 100%;
+background: transparent;
+}
+user agent stylesheetdiv {
+display: block;
+}
+Inherited from body#body.ltr.webkit.webkit-537.site-center-aligned.site-as-giant-card.appbar-hidden.appbar-flexwatch.appbar-flexwatch-540.not-nirvana-dogfood.hitchhiker-enabled.exp-css-ellipsis.page-loaded
+body {
+background: #f1f1f1;
+font-family: arial,sans-serif;
+font-size: 13px;
+}
+body {
+line-height: 1;
+text-align: left;
+text-align: start;
+}
+Inherited from html
+canvas, caption, center, cite, code, dd, del, dfn, div, dl, dt, em, embed, fieldset, font, form, h1, h2, h3, h4, h5, h6, hr, html, i, iframe, img, ins, kbd, label, legend, li, menu, object, ol, p, pre, q, s, samp, small, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, u, ul, var {
+margin: 0;
+padding: 0;
+border: 0;
+font-size: 100%;
+background: transparent;
+}
